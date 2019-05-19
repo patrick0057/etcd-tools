80b5defc19fe44cd184358588f9b628a14e48abd
patrick0057 <44879867+patrick0057@users.noreply.github.com>
2019-05-19 13:04:41 -0600

    Update etcd-join.sh

--- etcd-join.sh
+++ etcd-join.sh
@@ -3,6 +3,14 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 USAGE='Usage: ./etcd-join.sh <ssh user> <remote etcd IP> [path to ssh key for remote box]'
+rootcmd () {
+    if [[ $EUID -ne 0 ]]; then
+   echo "${green}Running as non root user, issuing command with sudo.${reset}" 
+   sudo $1
+    else 
+     $1
+fi
+}
 if [[ $1 == '' ]] || [[ $2 == '' ]] || [[ $@ =~ " -h" ]] || [[ $@ =~ " --help" ]]
  then
  echo "${green}${USAGE}${reset}"
@@ -10,7 +18,7 @@ if [[ $1 == '' ]] || [[ $2 == '' ]] || [[ $@ =~ " -h" ]] || [[ $@ =~ " --help" ]
 fi
 if [ "$(docker ps -a --filter "name=^/etcd-join$" --format '{{.Names}}')" == "etcd-join" ]
 then
-    docker remove etcd-join
+    docker rm -f etcd-join
 fi
 REMOTE_SSH_USER=$1
 REMOTE_SSH_IP=$2
@@ -25,7 +33,7 @@ then
     ssh -o StrictHostKeyChecking=no -l ${REMOTE_SSH_USER} $REMOTE_SSH_IP exit
     if [[ $? -ne 0 ]]
     then
-        echo ${green}Unable to connect to remote SSH host, aborting script!${reset}
+        echo ${green}Unable to connect to remote SSH host, aborting script!  Did you set your ssh key\?${reset}
         echo
         echo "${green}${USAGE}${reset}"
         exit 1
@@ -43,6 +51,14 @@ then
 fi
 echo ${green}SSH test succesful.${reset}
 echo
+sshcmd () {
+if [[ ${#REMOTE_SSH_KEY} == 0 ]]
+then
+    ssh -o StrictHostKeyChecking=no -l ${REMOTE_SSH_USER} $REMOTE_SSH_IP $1
+    else
+    ssh -o StrictHostKeyChecking=no -i ${REMOTE_SSH_KEY} -l ${REMOTE_SSH_USER} $REMOTE_SSH_IP $1
+fi
+}
 export $(docker inspect etcd -f '{{.Config.Env}}'| sed 's/[][]//g')
 docker inspect etcd &> /dev/null
 if [[ $? -ne 0 ]]
@@ -91,13 +107,7 @@ docker stop etcd
 echo ${green}Waiting 11 seconds for etcd to stop${reset}
 sleep 11
 echo ${red}Moving old etcd data directory /var/lib/etcd to /var/lib/etcd-old--${ETCD_BACKUP_TIME}${reset}
-
-if [[ $EUID -ne 0 ]]; then
-   echo "${green}Running as non root user, issuing command with sudo.${reset}" 
-   sudo mv /var/lib/etcd /var/lib/etcd-old--${ETCD_BACKUP_TIME}
-    else 
-    mv /var/lib/etcd /var/lib/etcd-old--${ETCD_BACKUP_TIME}
-fi
+rootcmd "mv /var/lib/etcd /var/lib/etcd-old--${ETCD_BACKUP_TIME}"
 
 #ls -lash /var/lib/etcd
 sleep 2
@@ -114,15 +124,20 @@ INITIAL_CLUSTER=$(sed  's,^.*--initial-cluster=.*\('"$ETCD_NAME"'\)=\([^,^ ]*\).
 INITIAL_CLUSTER_TOKEN=$(sed  's,^.*initial-cluster-token=\([^ ]*\).*,\1,g' <<< $RUNLIKE)
 ADVERTISE_CLIENT_URLS=$(sed  's,^.*advertise-client-urls=\([^ ]*\).*,\1,g' <<< $RUNLIKE)
 
-
-ETCD_ADD_MEMBER_CMD="etcdctl --cacert $ETCDCTL_CACERT --cert $ETCDCTL_CERT --key ${ETCDCTL_KEY} member add ${ETCD_NAME} --peer-urls=${INITIAL_ADVERTISE_PEER_URL}"
-echo ${red}Connecting to remote etcd and issuing add member command${reset}
-if [[ ${#REMOTE_SSH_KEY} == 0 ]]
+#CHECK IF WE NEED TO ADD --endpoints TO THE COMMAND
+REQUIRE_ENDPOINT=$(sshcmd "docker exec etcd netstat -lpna | grep \:2379 | grep tcp | grep LISTEN | tr -s ' ' | cut -d' ' -f4")
+if [[ $REQUIRE_ENDPOINT =~ ":::" ]]
 then
-    export $(ssh -o StrictHostKeyChecking=no -l ${REMOTE_SSH_USER} $REMOTE_SSH_IP docker exec etcd ${ETCD_ADD_MEMBER_CMD} | grep ETCD_INITIAL_CLUSTER)
+    echo "${green} etcd is listening on ${REQUIRE_ENDPOINT}, no need to pass --endpoints${reset}"
+    ETCD_ADD_MEMBER_CMD="etcdctl --cacert $ETCDCTL_CACERT --cert $ETCDCTL_CERT --key ${ETCDCTL_KEY} member add ${ETCD_NAME} --peer-urls=${INITIAL_ADVERTISE_PEER_URL}"
     else
-    export $(ssh -o StrictHostKeyChecking=no -i ${REMOTE_SSH_KEY} -l ${REMOTE_SSH_USER} $REMOTE_SSH_IP docker exec etcd ${ETCD_ADD_MEMBER_CMD} | grep ETCD_INITIAL_CLUSTER)
+        echo "${green} etcd is only listening on ${REQUIRE_ENDPOINT}, we need to pass --endpoints${reset}"
+        ETCD_ADD_MEMBER_CMD="etcdctl --cacert $ETCDCTL_CACERT --cert $ETCDCTL_CERT --key ${ETCDCTL_KEY} member --endpoints ${REQUIRE_ENDPOINT} add ${ETCD_NAME} --peer-urls=${INITIAL_ADVERTISE_PEER_URL}"
 fi
+
+echo ${red}Connecting to remote etcd and issuing add member command${reset}
+export $(sshcmd "docker exec etcd ${ETCD_ADD_MEMBER_CMD} | grep ETCD_INITIAL_CLUSTER")
+echo "${red}ETCD_ADD_MEMBER_CMD has been set to ${ETCD_ADD_MEMBER_CMD} <-If this is blank etcd-join will fail${reset}"
 
 
 RESTORE_RUNLIKE='docker run
@@ -165,7 +180,7 @@ echo $RESTORE_RUNLIKE
 
 
 
-echo ${green}Launching clean etcd in order to rejoin cluster${reset}
+echo ${green}Launching etcd-join${reset}
 eval $RESTORE_RUNLIKE
 echo
 
@@ -214,15 +229,19 @@ then
         exit 1
 fi
 echo "${green}etcd is running, checking etcd things before exitting.${reset}"
-
-if [[ ${#REMOTE_SSH_KEY} == 0 ]]
+if [[ $REQUIRE_ENDPOINT =~ ":::" ]]
 then
-    ssh -o StrictHostKeyChecking=no -l ${REMOTE_SSH_USER} $REMOTE_SSH_IP docker exec etcd etcdctl member list
+    echo "${green} etcd is listening on ${REQUIRE_ENDPOINT}, no need to pass --endpoints${reset}"
+    sshcmd "docker exec etcd etcdctl member list"
     else
-        ssh -o StrictHostKeyChecking=no -i ${REMOTE_SSH_KEY} -l ${REMOTE_SSH_USER} $REMOTE_SSH_IP docker exec etcd etcdctl member list
+        echo "${green} etcd is only listening on ${REQUIRE_ENDPOINT}, we need to pass --endpoints${reset}"
+        sshcmd "docker exec etcd etcdctl --endpoints ${REQUIRE_ENDPOINT} member list"
 fi
 
 #curl -k --cacert $ETCDCTL_CACERT --cert $ETCDCTL_CERT --key ${ETCDCTL_KEY} https://18.191.139.66:2379/v3beta/cluster/member/add -XPOST -H "Content-Type: application/json" -d '{"peerURLs":["${INITIAL_ADVERTISE_PEER_URL}"]}'
 
 echo ${red}Setting etcd restart policy to always restart${reset}
 docker update --restart=always etcd
+
+echo ${red}Restarting kubelet and kube-apiserver if they exist${reset}
+docker restart kubelet kube-apiserver
