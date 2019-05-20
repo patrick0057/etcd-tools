979fdaf19f54e8c5f049aa823f0f4ee7e489cdb8
patrick0057 <44879867+patrick0057@users.noreply.github.com>
2019-05-19 19:53:43 -0600

    Update etcd-join.sh

--- etcd-join.sh
+++ etcd-join.sh
@@ -2,100 +2,81 @@
 red=$(tput setaf 1)
 green=$(tput setaf 2)
 reset=$(tput sgr0)
-USAGE='Usage: ./etcd-join.sh <ssh user> <remote etcd IP> [path to ssh key for remote box]'
+ETCD_BACKUP_TIME=$(date +%Y-%m-%d--%H%M%S)
+
 rootcmd() {
-    if [[ $EUID -ne 0 ]]; then
-        echo "${green}Running as non root user, issuing command with sudo.${reset}"
-        sudo $1
-    else
-        $1
-    fi
+        if [[ $EUID -ne 0 ]]; then
+                echo "${green}Running as non root user, issuing command with sudo.${reset}"
+                sudo $1
+        else
+                $1
+        fi
 }
-sshcmd() {
-    if [[ ${#REMOTE_SSH_KEY} == 0 ]]; then
-        ssh -o StrictHostKeyChecking=no -l ${REMOTE_SSH_USER} $REMOTE_SSH_IP $1
-    else
-        ssh -o StrictHostKeyChecking=no -i ${REMOTE_SSH_KEY} -l ${REMOTE_SSH_USER} $REMOTE_SSH_IP $1
-    fi
-}
+
+if [[ $? -ne 0 ]]; then
+        echo ${green}Setting timestamp failed, does the \"date\" command exist\?${reset}
+        exit 1
+fi
+if [ -d "/opt/rke/etcd" ]; then
+        echo ${green}/opt/rke/etcd exists, moving it to /opt/rke/etcd--${ETCD_BACKUP_TIME}.${reset}
+        rootcmd "mv /opt/rke/etcd /opt/rke/etcd--${ETCD_BACKUP_TIME}"
+fi
+if [ ! "$(docker ps -a --filter "name=^/etcd$" --format '{{.Names}}')" == "etcd" ]; then
+        echo "${green}etcd container does not exist, script cannot proceed${reset}"
+        exit 1
+fi
+if [ "$(docker ps -a --filter "name=^/etcd-restore$" --format '{{.Names}}')" == "etcd-restore" ]; then
+        echo "${green}etcd-restore container exists, please remove this container before running the script${reset}"
+        exit 1
+fi
+if [ "$(docker ps -a --filter "name=^/etcd-reinit$" --format '{{.Names}}')" == "etcd-reinit" ]; then
+        echo "${green}etcd-reinit container exists, please remove this container before running the script${reset}"
+        exit 1
+fi
 #Help menu
-if [[ $1 == '' ]] || [[ $2 == '' ]] || [[ $@ =~ " -h" ]] || [[ $1 == "-h" ]] || [[ $@ =~ " --help" ]] || [[ $1 =~ "--help" ]]; then
-    echo "${green}${USAGE}${reset}"
-    exit 1
+USAGE='Usage: ./restore-etcd-single.sh </path/to/snapshot>'
+if [[ $1 == '' ]] || [[ $@ =~ " -h" ]] || [[ $1 == "-h" ]] || [[ $@ =~ " --help" ]] || [[ $1 =~ "--help" ]]; then
+        echo "${green}${USAGE}${reset}"
+        exit 1
 fi
-if [ "$(docker ps -a --filter "name=^/etcd-join$" --format '{{.Names}}')" == "etcd-join" ]; then
-    docker rm -f etcd-join
+
+RESTORE_SNAPSHOT=$1
+#check if image exists
+ls -lash $RESTORE_SNAPSHOT
+if [[ $? -ne 0 ]]; then
+        echo ${green}Image $RESTORE_SNAPSHOT does not exist, aborting script!${reset}
+        exit 1
 fi
+#move stale snapshot out of way if it exists
+if [ -f "/etc/kubernetes/snapshot.db" ]; then
+        echo ${red}Found stale snapshot at /etc/kubernetes/snapshot.db, moving it out of the way to /etc/kubernetes/snapshot.db--${ETCD_BACKUP_TIME}${reset}
+        rootcmd "mv /etc/kubernetes/snapshot.db /etc/kubernetes/snapshot.db--${ETCD_BACKUP_TIME}"
+fi
+#copy snapshot into place
+echo ${red}Copying $RESTORE_SNAPSHOT to /etc/kubernetes/snapshot.db ${reset}
+rootcmd "cp $RESTORE_SNAPSHOT /etc/kubernetes/snapshot.db"
+if [[ $? -ne 0 ]]; then
+        echo ${green}Failed to copy $RESTORE_SNAPSHOT to /etc/kubernetes/snapshot.db, aborting script!${reset}
+        exit 1
+fi
+
 #check for runlike container
-echo ${green}Gathering information about your etcd container with runlike${reset}
 RUNLIKE=$(docker run --rm -v /var/run/docker.sock:/var/run/docker.sock patrick0057/runlike etcd)
 if [[ $? -ne 0 ]]; then
-    echo ${green}runlike container failed to run, aborting script!${reset}
-    exit 1
-fi
-
-REMOTE_SSH_USER=$1
-REMOTE_SSH_IP=$2
-REMOTE_SSH_KEY=$3
-echo ${green}Verifying SSH connections...${reset}
-echo ssh user: ${REMOTE_SSH_USER}
-echo ssh ip: ${REMOTE_SSH_IP}
-echo ssh key: ${REMOTE_SSH_KEY}
-#echo length ${#REMOTE_SSH_KEY}
-if [[ ${#REMOTE_SSH_KEY} == 0 ]]; then
-    ssh -o StrictHostKeyChecking=no -l ${REMOTE_SSH_USER} $REMOTE_SSH_IP exit
-    if [[ $? -ne 0 ]]; then
-        echo ${green}Unable to connect to remote SSH host, aborting script! Did you set your ssh key\?${reset}
-        echo
-        echo "${green}${USAGE}${reset}"
+        echo ${green}runlike container failed to run, aborting script!${reset}
         exit 1
-
-    fi
-else
-    ssh -o StrictHostKeyChecking=no -i ${REMOTE_SSH_KEY} -l ${REMOTE_SSH_USER} $REMOTE_SSH_IP exit
-    if [[ $? -ne 0 ]]; then
-        echo ${green}Unable to connect to remote SSH host, aborting script!${reset}
-        echo
-        echo "${green}${USAGE}${reset}"
-        exit 1
-    fi
 fi
-echo ${green}SSH test succesful.${reset}
-echo
-
-#Check if etcd is actually running on the remote server
-echo ${green}Checking to see if etcd is actually running on the remote host ${reset}
-REMOTE_ETCD_RUNNING=$(sshcmd "docker ps --filter 'name=^/etcd$' --format '{{.Names}}'")
-if [[ ! ${REMOTE_ETCD_RUNNING} == 'etcd' ]]; then
-    echo ${green}etcd is not running on the remote host! Check that you have the correct host then try again.${reset}
-    exit 1
-fi
-echo ${green}etcd is running on the remote host, excellent!
-echo
-
-export $(docker inspect etcd -f '{{.Config.Env}}' | sed 's/[][]//g')
-docker inspect etcd &>/dev/null
-if [[ $? -ne 0 ]]; then
-    echo ${green}Uable to inspect the etcd container, does it still exist? Aborting script!${reset}
-    echo
-    echo "${green}${USAGE}${reset}"
-    exit 1
-fi
-echo ${green}I was able to inspect the etcd container! Script will proceed...${reset}
-echo
 
 echo ${red}Setting etcd restart policy to never restart \"no\"${reset}
 docker update --restart=no etcd
-
-ETCD_BACKUP_TIME=$(date +%Y-%m-%d--%H%M%S)
-
-echo ${red}Stopping etcd container${reset}
-docker stop etcd
+echo ${red}Renaming original etcd container to etcd-old--${ETCD_BACKUP_TIME}
+docker rename etcd etcd-old--${ETCD_BACKUP_TIME}
+echo ${red}Stopping original etcd container${reset}
+docker stop etcd-old--${ETCD_BACKUP_TIME}
 
 echo ${red}Moving old etcd data directory /var/lib/etcd to /var/lib/etcd-old--${ETCD_BACKUP_TIME}${reset}
 rootcmd "mv /var/lib/etcd /var/lib/etcd-old--${ETCD_BACKUP_TIME}"
 
-ETCD_NAME=$(sed 's,^.*name=\([^ ]*\).*,\1,g' <<<$RUNLIKE)
 ETCD_HOSTNAME=$(sed 's,^.*--hostname=\([^ ]*\).*,\1,g' <<<$RUNLIKE)
 ETCDCTL_ENDPOINT="https://0.0.0.0:2379"
 ETCDCTL_CACERT=$(sed 's,^.*ETCDCTL_CACERT=\([^ ]*\).*,\1,g' <<<$RUNLIKE)
@@ -105,25 +86,11 @@ ETCD_VERSION=$(sed 's,^.*rancher/coreos-etcd:\([^ ]*\).*,\1,g' <<<$RUNLIKE)
 INITIAL_ADVERTISE_PEER_URL=$(sed 's,^.*initial-advertise-peer-urls=\([^ ]*\).*,\1,g' <<<$RUNLIKE)
 ETCD_NAME=$(sed 's,^.*name=\([^ ]*\).*,\1,g' <<<$RUNLIKE)
 INITIAL_CLUSTER=$(sed 's,^.*--initial-cluster=.*\('"$ETCD_NAME"'\)=\([^,^ ]*\).*,\1=\2,g' <<<$RUNLIKE)
+ETCD_SNAPSHOT_LOCATION='/etc/kubernetes/snapshot.db'
 INITIAL_CLUSTER_TOKEN=$(sed 's,^.*initial-cluster-token=\([^ ]*\).*,\1,g' <<<$RUNLIKE)
-ADVERTISE_CLIENT_URLS=$(sed 's,^.*advertise-client-urls=\([^ ]*\).*,\1,g' <<<$RUNLIKE)
-
-#CHECK IF WE NEED TO ADD --endpoints TO THE COMMAND
-REQUIRE_ENDPOINT=$(sshcmd "docker exec etcd netstat -lpna | grep \:2379 | grep tcp | grep LISTEN | tr -s ' ' | cut -d' ' -f4")
-if [[ $REQUIRE_ENDPOINT =~ ":::" ]]; then
-    echo "${green}etcd is listening on ${REQUIRE_ENDPOINT}, no need to pass --endpoints${reset}"
-    ETCD_ADD_MEMBER_CMD="etcdctl --cacert $ETCDCTL_CACERT --cert $ETCDCTL_CERT --key ${ETCDCTL_KEY} member add ${ETCD_NAME} --peer-urls=${INITIAL_ADVERTISE_PEER_URL}"
-else
-    echo "${green}etcd is only listening on ${REQUIRE_ENDPOINT}, we need to pass --endpoints${reset}"
-    ETCD_ADD_MEMBER_CMD="etcdctl --cacert $ETCDCTL_CACERT --cert $ETCDCTL_CERT --key ${ETCDCTL_KEY} member --endpoints ${REQUIRE_ENDPOINT} add ${ETCD_NAME} --peer-urls=${INITIAL_ADVERTISE_PEER_URL}"
-fi
-
-echo ${red}Connecting to remote etcd and issuing add member command${reset}
-export $(sshcmd "docker exec etcd ${ETCD_ADD_MEMBER_CMD} | grep ETCD_INITIAL_CLUSTER")
-echo "${red}ETCD_INITIAL_CLUSTER has been set to ${ETCD_INITIAL_CLUSTER} ${green}<-If this is blank etcd-join will fail${reset}"
 
 RESTORE_RUNLIKE='docker run
---name=etcd-join
+--name=etcd-restore
 --hostname='$ETCD_HOSTNAME'
 --env="ETCDCTL_API=3"
 --env="ETCDCTL_ENDPOINT='$ETCDCTL_ENDPOINT'"
@@ -136,64 +103,88 @@ RESTORE_RUNLIKE='docker run
 --volume="/opt/rke:/opt/rke:z"
 --network=host
 --label io.rancher.rke.container.name="etcd"
---detach=true rancher/coreos-etcd:'$ETCD_VERSION' /usr/local/bin/etcd
---peer-client-cert-auth
---client-cert-auth
---initial-cluster='${ETCD_INITIAL_CLUSTER}'
---initial-cluster-state=existing
---trusted-ca-file='${ETCDCTL_CACERT}'
---listen-client-urls=https://0.0.0.0:2379
---initial-advertise-peer-urls='${INITIAL_ADVERTISE_PEER_URL}'
---listen-peer-urls=https://0.0.0.0:2380
---heartbeat-interval=500
---election-timeout=5000
---data-dir=/var/lib/rancher/etcd/
---initial-cluster-token='${INITIAL_CLUSTER_TOKEN}'
---peer-cert-file='${ETCDCTL_CERT}'
---peer-key-file='${ETCDCTL_KEY}'
---name='${ETCD_NAME}'
---advertise-client-urls='${ADVERTISE_CLIENT_URLS}'
---peer-trusted-ca-file='${ETCDCTL_CACERT}'
---cert-file='${ETCDCTL_CERT}'
---key-file='${ETCDCTL_KEY}''
-echo ${green}Running the following command:${reset}
+-ti rancher/coreos-etcd:'$ETCD_VERSION' /usr/local/bin/etcdctl snapshot restore '$ETCD_SNAPSHOT_LOCATION'
+--initial-advertise-peer-urls='$INITIAL_ADVERTISE_PEER_URL'
+--initial-cluster='$INITIAL_CLUSTER'
+--initial-cluster-token='$INITIAL_CLUSTER_TOKEN'
+--data-dir=/opt/rke/etcd
+--name='$ETCD_NAME''
+
+#RESTORE ETCD
+echo ${red}Restoring etcd snapshot${reset}
 echo $RESTORE_RUNLIKE
-
-echo ${green}Launching etcd-join${reset}
 eval $RESTORE_RUNLIKE
-echo
+#echo ${green}Sleeping for 10 seconds so etcd can do its restore${reset}
+#sleep 10
 
-echo ${green}Script sleeping for 10 seconds${reset}
+echo ${red}Stopping etcd-restore container${reset}
+docker stop etcd-restore
+
+echo ${red}Moving restored etcd directory in place${reset}
+rootcmd "mv /opt/rke/etcd /var/lib/"
+
+echo ${red}Deleting etcd-restore container${reset}
+docker rm -f etcd-restore
+
+#INITIALIZE NEW RUNLIKE
+NEW_RUNLIKE=$RUNLIKE
+
+#ADD --force-new-cluster
+NEW_RUNLIKE=$(sed 's,^\(.*'$ETCD_VERSION' \)\([^ ]*\)\(.*\),\1\2 --force-new-cluster\3,g' <<<$NEW_RUNLIKE)
+
+#REMOVE OTHER ETCD NODES FROM --initial-cluster
+ORIG_INITIAL_CLUSTER=$(sed 's,^.*initial-cluster=\([^ ]*\).*,\1,g' <<<$RUNLIKE)
+NEW_RUNLIKE=$(sed 's`'"$ORIG_INITIAL_CLUSTER"'`'"$INITIAL_CLUSTER"'`g' <<<$NEW_RUNLIKE)
+
+#CHANGE NAME TO etcd-reinit
+NEW_RUNLIKE=$(sed 's`'--name=etcd'`'--name=etcd-reinit'`g' <<<$NEW_RUNLIKE)
+
+#REINIT ETCD
+echo ${red}Running etcd-reinit${reset}
+echo $NEW_RUNLIKE
+eval $NEW_RUNLIKE
+echo ${green}Sleeping for 10 seconds so etcd can do reinit things${reset}
 sleep 10
 
-if [ ! "$(docker ps --filter "name=^/etcd-join$" --format '{{.Names}}')" == "etcd-join" ]; then
-    echo "${green} etcd-join is not running, something went wrong.  Make sure the etcd cluster only has healthy and online members then try again.${reset}"
-    exit 1
-fi
+#echo ${green}Tailing last 40 lines of etcd-reinit${reset}
+#docker logs etcd-reinit --tail 40
 
-echo ${green}etcd-join appears to be running still, this is a good sign. Proceeding with cleanup.${reset}
-echo ${red}Stopping etcd-join${reset}
-docker stop etcd-join
-echo ${red}Deleting etcd-join${reset}
-docker rm etcd-join
-echo ${red}Starting etcd${reset}
-docker start etcd
+#STOP AND REMOVE etcd-reinit
+echo ${red}Stopping and removing etcd-reinit${reset}
+docker stop etcd-reinit
+docker rm -f etcd-reinit
 
-if [ ! "$(docker ps --filter "name=^/etcd$" --format '{{.Names}}')" == "etcd" ]; then
-    echo "${green}etcd is not running, something went wrong.${reset}"
-    exit 1
-fi
-echo "${green}etcd is running, checking etcd things before exitting.${reset}"
-if [[ $REQUIRE_ENDPOINT =~ ":::" ]]; then
-    echo "${green}etcd is listening on ${REQUIRE_ENDPOINT}, no need to pass --endpoints${reset}"
-    sshcmd "docker exec etcd etcdctl member list"
-else
-    echo "${green}etcd is only listening on ${REQUIRE_ENDPOINT}, we need to pass --endpoints${reset}"
-    sshcmd "docker exec etcd etcdctl --endpoints ${REQUIRE_ENDPOINT} member list"
-fi
+#CHANGE NAME BACK TO etcd
+NEW_RUNLIKE=$(sed 's`'--name=etcd-reinit'`'--name=etcd'`g' <<<$NEW_RUNLIKE)
+
+#REMOVE --force-new-cluster
+NEW_RUNLIKE=$(sed 's`--force-new-cluster ``g' <<<$NEW_RUNLIKE)
+
+#FINALLY RUN NEW SHINY RESTORED ETCD
+echo ${red}Launching shiny new etcd${reset}
+echo $NEW_RUNLIKE
+eval $NEW_RUNLIKE
+
+echo ${red}Restarting kubelet and kube-apiserver if they exist${reset}
+docker restart kubelet kube-apiserver
+
+echo ${red}Removing /etc/kubernetes/snapshot.db${reset}
+#rootcmd "mv /etc/kubernetes/snapshot.db /etc/kubernetes/snapshot.db--${ETCD_BACKUP_TIME}"
+rootcmd "rm -f /etc/kubernetes/snapshot.db"
 
 echo ${red}Setting etcd restart policy to always restart${reset}
 docker update --restart=always etcd
 
-echo ${red}Restarting kubelet and kube-apiserver if they exist${reset}
-docker restart kubelet kube-apiserver
+#PRINT OUT MEMBER LIST
+#CHECK IF WE NEED TO ADD --endpoints TO THE COMMAND
+REQUIRE_ENDPOINT=$(docker exec etcd netstat -lpna | grep \:2379 | grep tcp | grep LISTEN | tr -s ' ' | cut -d' ' -f4)
+if [[ $REQUIRE_ENDPOINT =~ ":::" ]]; then
+        echo "${green}etcd is listening on ${REQUIRE_ENDPOINT}, no need to pass --endpoints${reset}"
+        docker exec etcd etcdctl member list
+else
+        echo "${green}etcd is only listening on ${REQUIRE_ENDPOINT}, we need to pass --endpoints${reset}"
+        docker exec etcd etcdctl --endpoints ${REQUIRE_ENDPOINT} member list
+fi
+
+echo ${green}Single restore has completed, please be sure to restart kubelet and kube-apiserver on other nodes.${reset}
+echo ${green}If you are planning to rejoin another node to this etcd cluster you\'ll want to use etcd-join.sh on that node${reset}
